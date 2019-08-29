04171c6604977ff0556c99b561bffce4d3302c48
zlawson13 <zach@executivedsllc.com>
2019-08-29 11:32:20 -0500

    Updating a miss code

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import CardList from './CardList.js'
 import * as serviceWorker from './serviceWorker';
-import { robots } from './robots'
+import {
+    robots
+} from './robots'
 
-ReactDOM.render(
-        <CardList robots={robots} />
-    ,  document.getElementById('root'));
+ReactDOM.render( <
+    CardList robots = {
+        robots
+    }
+    />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
